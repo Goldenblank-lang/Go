46e7eaa6a2fc90aadf556f1fa8a0ee07a99020c9
Goldenblank-lang <m0592294426@gmail.com>
2026-03-19 10:15:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'golden-blank-v1';
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './admin.html',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400&family=Tajawal:wght@300;400;500;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => cached))
+    );
+});
